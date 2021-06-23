dbdf9eb4a91bf2116c3dce366d54c67c2e39fddb
Marcin <mkrolczyk@griddynamics.com>
2021-06-23 16:11:12 +0200

    added plain sql implementation of top 10 campaigns with biggest revenue and most popular channel in each campaign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql import SQLContext
 from pyspark.sql.functions import udf
 from pyspark.sql.functions import explode, split, col, create_map, translate, collect_list, monotonically_increasing_id, expr, round, to_timestamp, when, lit, row_number, sum, max
 from pyspark.sql.window import Window
@@ -9,8 +8,6 @@ spark = SparkSession \
     .builder \
     .appName("Capstone project 1") \
     .getOrCreate()
-
-sqlContext = SQLContext(spark)
 
 MOBILE_DATA_PATH = "capstone-dataset/mobile_app_clickstream/*.csv.gz"
 PURCHASES_DATA_PATH = "capstone-dataset/user_purchases/*.csv.gz"
@@ -78,17 +75,13 @@ def main():
         .orderBy("userId", "eventTime") \
         .cache()
 
-    # print("structured_mobile_data-------------------------------------")
     # structured_mobile_data.show(20, False)
 
     aggr_data = structured_mobile_data \
         .groupBy("userId", "session_id") \
         .agg(collect_list("eventType").alias("sessionId"), collect_list("attributes").alias("temp")) \
-        .select("userId", "sessionId", expr(
-        'aggregate(slice(temp, 2, size(temp)), temp[0], (acc, element) -> map_concat(acc, element))').alias(
-        "attributes"))
+        .select("userId", "sessionId", expr('aggregate(slice(temp, 2, size(temp)), temp[0], (acc, element) -> map_concat(acc, element))').alias("attributes"))
 
-    # print("aggr_data-------------------------------------")
     # aggr_data.show(20, False)
 
     joined_data = structured_mobile_data \
@@ -98,7 +91,6 @@ def main():
         .sort(col("userId"), col("eventTime")) \
         .cache()
 
-    # print("joined_data-------------------------------------")
     # joined_data.show(20, False)
 
     target_dataframe = joined_data \
@@ -112,28 +104,62 @@ def main():
                 col("attributes")["campaign_id"].alias("campaignId"),
                 col("attributes")["channel_id"].alias("channelIid")) \
         .withColumn("sessionId", monotonically_increasing_id() + 1) \
-        .cache()
-
-    print("target_dataframe-------------------------------------")
-    target_dataframe.printSchema()
-    target_dataframe.show(20, False)
+        .cache() \
 
 
-    target_dataframe.show(10, False)
-    target_dataframe.printSchema()
+    print("-------------target_dataframe-------------")
+    # target_dataframe.printSchema()
+    # target_dataframe.show(10, False)
+    target_dataframe.registerTempTable("target_dataframe")  # comment or uncomment depending on using pyspark dataframe API or plain sql
 
+    print("-------------task_2.1-------------")
 
+    # SQL version
+    biggest_revenue = spark.sql(""
+              "SELECT campaignId, round(AGGREGATE(billings, DOUBLE(0), (acc, x) -> acc + x), 2) as revenue "
+              "FROM ("
+                  "SELECT campaignId, collect_list(billingCost) as billings "
+                  "FROM (SELECT * "
+                            "FROM target_dataframe "
+                            "WHERE target_dataframe.isConfirmed == TRUE) "
+                  "GROUP BY campaignId) "
+              "ORDER BY revenue DESC LIMIT 10")
+
+    biggest_revenue.show()
+
+    ''' 
+    # dataframe API version
     biggest_revenue = target_dataframe \
         .filter(target_dataframe.isConfirmed == True) \
         .groupBy("campaignId") \
         .agg(collect_list(col("billingCost")).alias("billings")) \
         .select(col("campaignId"), round(expr('AGGREGATE(billings, DOUBLE(0), (acc, x) -> acc + x)'), 2).alias('revenue')) \
         .orderBy(col("revenue").desc()) \
+        .limit(10)
 
-    biggest_revenue.show(10, False)
+    biggest_revenue.show()
+    '''
+    print("-------------task_2.2-------------")
 
+    # SQL version
+    mst_popular_channel = \
+        spark.sql(""
+                "SELECT campaignId, channelIid, unique_sessions "
+                "FROM ("
+                    "SELECT campaignId, channelIid, unique_sessions, ROW_NUMBER() OVER(PARTITION BY campaignId ORDER BY unique_sessions DESC) as rnk "
+                    "FROM ("
+                        "SELECT campaignId, channelIid, COUNT(*) as unique_sessions "
+                        "FROM target_dataframe "
+                        "GROUP BY campaignId, channelIid) "
+                    ")"
+                "WHERE rnk == 1"
+                "")
 
+    mst_popular_channel.show(10, False)
+    '''
+    # dataframe API version
     w3 = Window.partitionBy("campaignId").orderBy(col("count").desc())
+    
     mst_popular_channel = target_dataframe \
         .groupBy("campaignId", "channelIid") \
         .count() \
@@ -142,7 +168,7 @@ def main():
         .withColumnRenamed("count", "unique_sessions")
 
     mst_popular_channel.show(10, False)
-
+    '''
 
 
 if __name__ == '__main__':
